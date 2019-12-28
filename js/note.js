94add3822d61fe5bbab44351ee0143953b42c4bb
Kevin Du <kkxandeer@gmail.com>
2019-12-28 13:10:48 +0800

    manual: [2019-12-28 Sat 13:10:48]

--- js/note.js
+++ js/note.js
@@ -2,14 +2,24 @@ window.addEventListener('load', function () {
     var parent = document.getElementById('content');
     parent.insertBefore(document.getElementById('preamble'), parent.firstElementChild);
 
+    if (document.body.classList.contains('home')) {
+        return;
+    }
+
     var cachedHash = '';
+    var TOC_HASH = '#table-of-contents';
     $('.open-sidebar')[0].addEventListener('click', function () {
         cachedHash = location.hash;
-        location.hash = '#table-of-contents';
+        location.hash = TOC_HASH;
     });
 
     $(".close-sidebar")[0].addEventListener('click', function () {
         location.hash = cachedHash;
     });
 
+    $('body')[0].addEventListener('click', function () {
+        if (location.hash === TOC_HASH) {
+            location.hash = cachedHash;
+        }
+    });
 });
