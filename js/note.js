551130617def95cc0d4a593b0140a2f0d4549082
Kevin Du <kkxandeer@gmail.com>
2019-12-28 12:51:51 +0800

    manual: [2019-12-28 Sat 12:51:51]

--- js/note.js
+++ js/note.js
@@ -1,4 +1,15 @@
 window.addEventListener('load', function () {
     var parent = document.getElementById('content');
     parent.insertBefore(document.getElementById('preamble'), parent.firstElementChild);
+
+    var cachedHash = '';
+    $('.open-sidebar')[0].addEventListener('click', function () {
+        cachedHash = location.hash;
+        location.hash = '#table-of-contents';
+    });
+
+    $(".close-sidebar")[0].addEventListener('click', function () {
+        location.hash = cachedHash;
+    });
+
 });
