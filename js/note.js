044b614ec7449091f833cc08addd7cff9f19ecfa
Kevin Du <kkxandeer@gmail.com>
2019-12-28 13:38:59 +0800

    manual: [2019-12-28 Sat 13:38:59]

--- js/note.js
+++ js/note.js
@@ -8,13 +8,20 @@ window.addEventListener('load', function () {
 
     var cachedHash = '';
     var TOC_HASH = '#table-of-contents';
-    $('.open-sidebar')[0].addEventListener('click', function () {
-        cachedHash = location.hash;
-        location.hash = TOC_HASH;
+    $('.open-sidebar')[0].addEventListener('click', function (e) {
+        if (location.hash !== TOC_HASH) {
+            cachedHash = location.hash;
+            location.hash = TOC_HASH;
+            e.stopPropagation();
+        }
     });
 
     $(".close-sidebar")[0].addEventListener('click', function () {
         location.hash = cachedHash;
+    });
+
+    $(TOC_HASH)[0].addEventListener('click', function (e) {
+        e.stopPropagation();
     });
 
     $('body')[0].addEventListener('click', function () {
