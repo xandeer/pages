58872e4db6a9c1eea11baa7102585af3d14c49f0
Kevin Du <kkxandeer@gmail.com>
2021-07-17 18:09:06 +0800

    manual: [2021-07-17 Sat 18:09:06]

---
+++ static/x.js
@@ -0,0 +1,5 @@
+document.querySelectorAll("img").forEach((item) => {
+	  item.addEventListener("click", (event) => {
+		      window.location.href = event.target.getAttribute("src");
+		    });
+});
